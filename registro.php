0928b021f31996a95621a4f8f9a50ec4c4dfda25
sebasmn98 <sebasmayorga98@gmail.com>
2020-06-10 15:28:25 -0500

    php2

--- registro.php
+++ registro.php
@@ -7,7 +7,7 @@ try {
     VALUES ($_GET['nom'], $_GET['ape'], $_GET['ced'], $_GET['tel'], $_GET['dir'], $_GET['con'])";
     // use exec() because no results are returned
     $conn->exec($sql);
-    echo "New record created successfully";
+    echo "Guardado satisfactorio";
   } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
   }
