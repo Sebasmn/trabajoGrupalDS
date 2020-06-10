30c7cdc16a0bd0eaf3cfe44e036b3d73eee27c73
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-06-10 16:37:42 -0500

    commit

--- registro.php
+++ registro.php
@@ -1,19 +1,31 @@
 <?php
  require 'conexion.php';
 // Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$sql = "INSERT INTO usuarios (NOMBRE, APELLIDO, CEDULA, TELEFONO, DIRECCION, CLAVE)
-VALUES('$_POST['nom']',' $_POST['ape']','$_POST['ced']','$_POST['tel']','$_POST['dir']','$_POST['con']')";
-if (!mysqli_query($conn,$sql))
-{
-die('Error: ' . mysqli_error($con));
-}
-echo "Thank you.";
+$NOMBRE = $_POST['nombre'];
+$APELLIDO = $_POST['apellido'];
+$CEDULA = $_POST['cedula'];
+$TELEFONO = $_POST['telefono'];
+$CLAVE = $_POST['clave'];
+$DIRECCION = $_POST['direccion'];
 
-mysqli_close($con);
 
-require 'conexion.php';
+try {
+   
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "  INSERT INTO usuarios 
+    (NOMBRE ,APELLIDO,CEDULA,TELEFONO,DIRECCION,CLAVE) 
+    VALUES('$NOMBRE','$APELLIDO','$CEDULA','$TELEFONO','$DIRECCION','$CLAVE');";
+
+    /*$sql = "INSERT INTO usuarios (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";*/
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Ingresado";
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  
+  $conn = null;
+
   ?>
