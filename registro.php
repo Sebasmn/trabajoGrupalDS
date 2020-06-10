6682c7e9f698a4e3a174c6107a83873ae29c33c8
sebasmn98 <sebasmayorga98@gmail.com>
2020-06-10 15:37:52 -0500

    sebas

--- registro.php
+++ registro.php
@@ -1,16 +1,18 @@
 <?php
 require 'conexion.php'; 
 try {
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "INSERT INTO usuarios (NOMBRE, APELLIDO, CEDULA, TELEFONO, DIRECCION, CLAVE)
-    VALUES ($_GET['nom'], $_GET['ape'], $_GET['ced'], $_GET['tel'], $_GET['dir'], $_GET['con'])";
+    VALUES ('$_GET['nom']', '$_GET['ape']', '$_GET['ced']', '$_GET['tel']', '$_GET['dir']', '$_GET['con']')";
     // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Guardado satisfactorio";
-  } catch(PDOException $e) {
+    if (mysqli_query($conn, $sql)) {
+      echo "Grabado exitoso";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+} catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
-  }
+}
   
   $conn = null;
   ?>
