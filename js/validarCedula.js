9f85283db53393bb45c29cde7feb71ce534931ba
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-06-10 23:12:13 -0500

    ASD

--- js/validarCedula.js
+++ js/validarCedula.js
@@ -1,3 +1,5 @@
+document.getElementById("myMap").style.display = "none";
+
 
 function validar(){
    // var cedula = prompt("Ingrese su cedula");
@@ -52,52 +54,13 @@ function validar(){
     }
 
     //MOSTRAR MAPA
- function mostrarMapa(){
-
-        
-let myMap = L.map('myMap').setView([51.505, -0.09], 13)
-
-L.tileLayer(`https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png`, {
-	maxZoom: 18,
-}).addTo(myMap);
-
-let marker = L.marker([51.5, -0.09]).addTo(myMap)
-
-let iconMarker = L.icon({
-    iconUrl: 'marker.png',
-    iconSize: [60, 60],
-    iconAnchor: [30, 60]
-})
-
-let marker2 = L.marker([51.51, -0.09], { icon: iconMarker }).addTo(myMap)
-
-myMap.doubleClickZoom.disable()
-myMap.on('dblclick', e => {
-  let latLng = myMap.mouseEventToLatLng(e.originalEvent);
-
-  L.marker([latLng.lat, latLng.lng], { icon: iconMarker }).addTo(myMap)
-})
-
-navigator.geolocation.getCurrentPosition(
-  (pos) => {
-    const { coords } = pos
-    const { latitude, longitude } = coords
-    L.marker([latitude, longitude], { icon: iconMarker }).addTo(myMap)
-
-    setTimeout(() => {
-      myMap.panTo(new L.LatLng(latitude, longitude))
-    }, 5000)
-  },
-  (error) => {
-    console.log(error)
-  },
-  {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  })
-
-   
+function esconder(){
+ 
+  document.getElementById("myMap").style.visibility = "hidden";
 }
     
-    
+
+function mostrar(){
+  
+  document.getElementById("myMap").style.visibility = "visible";
+}
