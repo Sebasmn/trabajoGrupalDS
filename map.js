572f826679da5fd2040ebc009dc890caa25a6962
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-06-10 20:46:34 -0500

    asd

--- map.js
+++ map.js
@@ -14,6 +14,28 @@ function success(pos) {
   console.log('Latitude : ' + crd.latitude);
   console.log('Longitude: ' + crd.longitude);
   console.log('More or less ' + crd.accuracy + ' meters.');
+
+
+
+  var latlng = new L.LatLng(crd.latitude, crd.longitude);
+
+  var mymap = L.map('myMap').setView(latlng, 14)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+}).addTo(mymap);
+
+  marker1 = L.marker(latlng).addTo(mymap);
+  mymap.on('click', function (e) {
+    if (marker1) {
+      mymap.removeLayer(marker1);
+    }
+    let latLng = mymap.mouseEventToLatLng(e.originalEvent);
+    marker1 = new L.marker([latLng.lat, latLng.lng],13.5).addTo(mymap);
+  });
+
+
+
+  
 };
 
 function error(err) {
@@ -25,7 +47,10 @@ navigator.geolocation.getCurrentPosition(success, error, options);
 
 
 
-navigator.geolocation.getCurrentPosition(function(location) {
+/*navigator.geolocation.getCurrentPosition(
+  
+  
+  function(location) {
   var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
 
   var mymap = L.map('myMap').setView(latlng, 14)
@@ -42,4 +67,4 @@ navigator.geolocation.getCurrentPosition(function(location) {
     marker1 = new L.marker([latLng.lat, latLng.lng],13.5).addTo(mymap);
   });
 
-});
+}         );*/
