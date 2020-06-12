601784e24c76993954179825a328e913ea7edbfb
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-06-11 20:19:27 -0500

    ASD

--- registro.php
+++ registro.php
@@ -7,26 +7,47 @@ $CEDULA = $_POST['cedula'];
 $TELEFONO = $_POST['telefono'];
 $CLAVE = $_POST['clave'];
 $DIRECCION = $_POST['direccion'];
-
+$EMAIL = $_POST['email'];
 
 try {
    
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "  INSERT INTO usuarios 
-    (NOMBRE ,APELLIDO,CEDULA,TELEFONO,DIRECCION,CLAVE) 
-    VALUES('$NOMBRE','$APELLIDO','$CEDULA','$TELEFONO','$DIRECCION','$CLAVE');";
+  $sql = $conn->prepare("  INSERT INTO usuarios 
+    (NOMBRE ,APELLIDO,CEDULA,'EMAIL',TELEFONO,DIRECCION,CLAVE) 
+     VALUES(:NOMBRE,
+    :APELLIDO,
+    :CEDULA,
+    :EMAIL,
+    :TELEFONO,
+    :DIRECCION,
+    :CLAVE);"); 
 
-    /*$sql = "INSERT INTO usuarios (firstname, lastname, email)
-    VALUES ('John', 'Doe', 'john@example.com')";*/
-    // use exec() because no results are returned
+$sql->execute([
+  'NOMBRE' => $NOMBRE,
+  'APELLIDO' =>$APELLIDO,
+  'CEDULA' => $CEDULA,
+  'EMAIL' =>$EMAIL,
+  'TELEFONO' => $TELEFONO,
+  'DIRECCION' => $DIRECCION,
+  'CLAVE' =>$CLAVE,
+]);
     $conn->exec($sql);
-    //echo "Ingresado";
-    header('Location:  register.php');
+    $_SESSION['user_id'] = NOMBRE+ " "+APELLIDO;
+    $usuario = $_SESSION['user_id'] ;
+    echo "<script type='text/javascript'>alert('$usuario');</script>";
+
+   
+  header('Location:  comprar.php');
+
+
+    //
+    
 
   } catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
   //  header('Location:  prueba.html');
+  header('Location:  loginFinal.html');
   }
   
   $conn = null;
